e3c18a794a2d422f5cf50718dedcdfe098b8f62d
inftp <12506549+inftp@users.noreply.github.com>
2020-04-23 22:47:54 +1000

    remove old code

--- lib/geocoder_backend_web/schema.ex
+++ lib/geocoder_backend_web/schema.ex
@@ -7,9 +7,6 @@ defmodule GeocoderBackendWeb.Schema do
     @desc "Get locations matching address"
     field :locations, list_of(:location) do
       arg :address, type: non_null(:string)
-
-      # resolve fn %{address: address}, _ ->
-      #   {:ok, for({_key, value} <- @fake_geocoder, value.address =~ address, into: [], do: value)}
       resolve(&Resolvers.LocationResolver.match/3)
     end
   end
